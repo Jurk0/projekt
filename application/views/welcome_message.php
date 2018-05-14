476fd55970acbb79cd43093da922aefd79cb69ca
Jurk0 <1klasov1@gmail.com>
2018-05-14 12:12:04 +0200

    nastavenie databázy

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,15 +5,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	<link rel="icon" href="../../../../favicon.ico">
+	
 
-	<title>Starter Template for Bootstrap</title>
+	<title>Turnaje</title>
 
 	<!-- Bootstrap core CSS -->
 	<link href="<?php echo base_url("assets\css\bootstrap.min.css");?>" rel="stylesheet">
 
 	<!-- Custom styles for this template -->
-	<link href="<?php echo base_url("assets\css\starter-template");?>" rel="stylesheet">
+	<link href="<?php echo base_url("assets\css\starter-template.css");?>" rel="stylesheet">
 </head>
 
 <body>
